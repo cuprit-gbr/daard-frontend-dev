e37149e810c354957625f6684e78295945918e02
Toni <toni.schoenbuchner@csgis.de>
2022-04-11 13:32:02 +0200

    Update live endpoints

--- v1/src/config/env.js
+++ v1/src/config/env.js
@@ -35,15 +35,15 @@ export default [
 	},
 	{
 		name: 'live',
-		hostname: 'daard-atlas.cuprit.net',
+		hostname: 'daard.dainst.org',
 		appTitle: 'Digital Atlas Of Ancient Rare Diseases',
 		showDevOutput: false,
-		baseUrl: 'https://dai-gn-test.csgis.de/',
-		restBase: 'https://dai-gn-test.csgis.de/daard',
-		loginUrl: 'https://dai-gn-test.csgis.de/o/authorize/?response_type=code&client_id=jqAODkFHbb54YDouEx7JAT16kJxelPSIeDkU2n5U&redirect_uri=%redirect_uri%&state=abc=scope=daard-api',
-		tokenUrl: 'https://dai-gn-test.csgis.de/o/token/',
+		baseUrl: 'https://daard.dainst.org/',
+		restBase: 'https://daard.dainst.org/daard',
+		loginUrl: 'https://daard.dainst.org/o/authorize/?response_type=code&client_id=jqAODkFHbb54YDouEx7JAT16kJxelPSIeDkU2n5U&redirect_uri=%redirect_uri%&state=abc=scope=daard-api',
+		tokenUrl: 'https://daard.dainst.org/o/token/',
 		tokenClientId: 'jqAODkFHbb54YDouEx7JAT16kJxelPSIeDkU2n5U',
-		registerUrl: 'https://dai-gn-test.csgis.de/account/signup/',
+		registerUrl: 'https://geoserver.dainst.org/account/signup/',
 	},
 	/*
 	{
